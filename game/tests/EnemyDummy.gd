7a5298b71d319fa3e05ca70f7981c7d3913862cc
nicoak <nicollasak@gmail.com>
2020-09-22 19:51:29 -0300

    Creación de Carpetas de SFX Enemy
    BR-4
    Creación de Carpetas de SFX Enemy

--- game/tests/EnemyDummy.gd
+++ game/tests/EnemyDummy.gd
@@ -4,6 +4,8 @@ export var hitpoints := 50
 export var bullet: PackedScene
 export var bullet_type := 1
 var bullet_speed := 500
+onready var shoot_test = $ShootTest
+onready var explosion_test = $ExplosionTest
 
 var can_shoot := true
 
@@ -18,6 +20,7 @@ func _process(delta):
 		owner.get_node("BulletsContainer").add_child(new_bullet)
 		can_shoot = false
 		$GunTimer.start()
+		shoot_test.play()
 
 
 func _on_area_entered(area):
@@ -25,8 +28,9 @@ func _on_area_entered(area):
 
 func check_hitpoints(damage):
 	hitpoints -= damage
-	if hitpoints <= 0:
-		queue_free()
+	if hitpoints == 0:
+		explosion_test.play()
+		#queue_free()
 
 
 func _on_GunTimer_timeout():
