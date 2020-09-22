a74d1e2b90b7391510a8a04b95d36a86012c5e34
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-09-22 12:55:05 -0300

    BR-4 y BR-6
    * BR-4 + BR-6
    - Testeando destruccion masiva de clases del enemigo

--- game/enemies/EnemyBase.gd
+++ game/enemies/EnemyBase.gd
@@ -3,26 +3,21 @@ extends Area2D
 
 #### Variables Export
 export var hitpoints := 50.0
-export var bullet: PackedScene
-export var bullet_speed := 400
+
 export var is_aimer := false
 
 #### Variables
-var can_shoot := false
 var player: Player
-var bullet_rot_correction := 0.0
 var speed := 0.0
 var path: Path2D
 var follow: PathFollow2D
 var allow_shoot := true
-var is_shooting := false
-var shoot_lines := {"shoot_on": false, "shoot_off": false}
 
 
 #### Variables Onready
 onready var shoot_sound := $ShootSFX
 onready var shoot_positions := $ShootPositions
-onready var bullet_container: Node
+onready var gun_timer := $GunTimer
 
 
 #### Setters y Getters
@@ -43,19 +38,14 @@ func _ready() -> void:
 	follow = PathFollow2D.new()
 	path.add_child(follow)
 	follow.loop = false
-	bullet_container = get_top_level().get_node("BulletsContainer")
+
 	if is_aimer:
 		get_player()
 
 
 func _process(delta) -> void:
 	move(delta)
-	
-	if is_aimer and not player == null:
-		aim_to_player()
-		
-	if can_shoot and allow_shoot:
-		shoot()
+
 
 
 func get_top_level() -> Node:
@@ -77,46 +67,20 @@ func move(delta: float) -> void:
 	follow.offset += speed * delta
 	position = follow.global_position
 	
-	if follow.unit_offset >= 1.0:
-		queue_free()
-	
-	if follow.unit_offset >= 0.15 and not is_shooting and not shoot_lines.shoot_on:
-		is_shooting = true
-		can_shoot = true
-		shoot_lines.shoot_on = true
-	
-	if follow.unit_offset >= 0.85 and is_shooting and not shoot_lines.shoot_off:
-		is_shooting = false
-		can_shoot = false
-		allow_shoot = false
-		shoot_lines.shoot_off = true
-
-func aim_to_player():
-	var dir = player.global_position - global_position
-	var rot = dir.angle()
-	var rot_look = rot - 1.57
-	bullet_rot_correction = rad2deg(rot) - 90.0
-	rotation = rot_look
+	check_shooting_status()
+	check_end_of_path()
 
 
-func shoot() -> void:
-	can_shoot = false
-	$GunTimer.start()
-	shoot_sound.play()
-	for i in range(shoot_positions.get_child_count()):
-		var new_bullet := bullet.instance()
-		new_bullet.create(
-				shoot_positions.get_child(i).global_position,
-				bullet_speed,
-				0.0,
-				shoot_positions.get_child(i).get_bullet_type(),
-				1.0,
-				shoot_positions.get_child(i).get_bullet_angle() + bullet_rot_correction)
-		bullet_container.add_child(new_bullet)
+func check_end_of_path():
+	pass
+
+
+func check_shooting_status():
+	pass
 
 
 func _on_GunTimer_timeout() -> void:
-	can_shoot = true
+	pass
 
 
 func _on_area_entered(area) -> void:
---
+++ game/enemies/EnemyShooter.gd
@@ -0,0 +1,70 @@
+class_name EnemyShooter
+extends EnemyBase
+
+#### Variables Export
+export var bullet: PackedScene
+export var bullet_speed := 400
+
+#### Variables
+var can_shoot := false
+var bullet_rot_correction := 0.0
+var is_shooting := false
+var shoot_lines := {"shoot_on": false, "shoot_off": false}
+var bullet_container: Node
+
+
+#### Metodos
+func _ready() -> void:
+	bullet_container = .get_top_level().get_node("BulletsContainer")
+
+
+func _process(delta: float) -> void:	
+	if is_aimer and not player == null:
+		aim_to_player()
+	if can_shoot and self.allow_shoot:
+		shoot()
+
+
+func shoot() -> void:
+	can_shoot = false
+	self.gun_timer.start()
+	self.shoot_sound.play()
+	for i in range(self.shoot_positions.get_child_count()):
+		var new_bullet := bullet.instance()
+		new_bullet.create(
+				self.shoot_positions.get_child(i).global_position,
+				bullet_speed,
+				0.0,
+				self.shoot_positions.get_child(i).get_bullet_type(),
+				1.0,
+				self.shoot_positions.get_child(i).get_bullet_angle() + bullet_rot_correction)
+		bullet_container.add_child(new_bullet)
+
+
+func check_shooting_status():
+	if self.follow.unit_offset >= 0.15 and not is_shooting and not shoot_lines.shoot_on:
+		is_shooting = true
+		can_shoot = true
+		shoot_lines.shoot_on = true
+	
+	if self.follow.unit_offset >= 0.85 and is_shooting and not shoot_lines.shoot_off:
+		is_shooting = false
+		can_shoot = false
+		self.allow_shoot = false
+		shoot_lines.shoot_off = true
+
+
+func aim_to_player() -> void:
+	var dir = player.global_position - global_position
+	var rot = dir.angle()
+	var rot_look = rot - 1.57
+	rotation = rot_look
+	bullet_rot_correction = rad2deg(rot) - 90.0
+
+
+func check_end_of_path() -> void:
+	if follow.unit_offset >= 1.0:
+		queue_free()
+
+func _on_GunTimer_timeout() -> void:
+	can_shoot = true
