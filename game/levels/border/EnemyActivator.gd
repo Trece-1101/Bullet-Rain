73adabd079b2616abb7b5b9963f4eb393fc86633
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-10-03 20:50:53 -0300

    BR-55
    * BR-55
    Creado e implementado mid boss

--- game/levels/border/EnemyActivator.gd
+++ game/levels/border/EnemyActivator.gd
@@ -4,4 +4,6 @@ extends Area2D
 func _on_area_entered(area: Area2D) -> void:	
 	if area is EnemyShooter:
 		var can_shoot:bool = area.get_inside_play_screen()
+		if area.get_is_boss():
+			can_shoot = false
 		area.set_inside_play_screen(not can_shoot)
