315474d7eeff696588ebe05c3887eaccc1c0e5c2
fernakasa <fernakasa@gmail.com>
2020-10-17 20:16:37 -0300

    Revert BR-13 BR-26

--- game/ui/menus/Menu.gd
+++ game/ui/menus/Menu.gd
@@ -1,20 +1,13 @@
 class_name Menu
 extends Control
 
+onready var video := $Video
+onready var animenu := $AnimationMenu
 
 #### Metodos
 func _ready() -> void:
-	get_button_connection()
+	video.play()
 
 
-func get_button_connection() -> void:
-	var button_container := get_tree().get_nodes_in_group("change_scene_button")
-	for button in button_container:
-		if button is ChangeSceneButton:
-	# warning-ignore:return_value_discarded
-			button.connect("pressed", self, "change_menu", [button.go_to_scene])
-
-
-func change_menu(menu: String) -> void:
-# warning-ignore:return_value_discarded
-	get_tree().change_scene(menu)
+func _on_AnimatedSprite_animation_finished():
+	animenu.play("playMenu")
