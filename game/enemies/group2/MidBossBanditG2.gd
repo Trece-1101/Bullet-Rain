7814abe73e9bc0747cce2fa3823c626a422f1516
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-11-04 20:53:38 -0300

    BR-62
    *BR-62
    Mid boss grupo 4 creado con su path

--- game/enemies/group2/MidBossBanditG2.gd
+++ game/enemies/group2/MidBossBanditG2.gd
@@ -1,8 +1,10 @@
+class_name MidBossBandit
 extends EnemyBandit
 
+export var orbitals_stats := {"speed": 1.5, "angle": 0, "distance": 300}
 
 func _on_MinionSpawner_timeout() -> void:
-	create_orbital(1.5, 0, 300)
+	create_orbital(orbitals_stats.speed, orbitals_stats.angle, orbitals_stats.distance)
 	$MinionSpawner.start()
 
 func die() -> void:
