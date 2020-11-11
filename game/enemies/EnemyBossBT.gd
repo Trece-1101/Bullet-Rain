07b55715438e17423d0ffc2e74e84965fc8895e0
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-11-11 11:32:16 -0300

    BR-69

--- game/enemies/EnemyBossBT.gd
+++ game/enemies/EnemyBossBT.gd
@@ -10,8 +10,13 @@ export var bullet_speed := 400.0
 export var test_shoot := false
 export(Array, NodePath) var indestructible_bullets
 export var start_position := Vector2.ZERO
+export var defense_position := Vector2.ZERO
+export var defense_time := 5.0
 export var shoot_rate := 1.0
+export var bomb_shoot_rate := 1.0
 export var is_aimer = true
+export(Array, PackedScene) var normal_minions
+export(Array, PackedScene) var critic_minions
 
 #### Variables
 var original_hitpoints: float
@@ -28,6 +33,8 @@ onready var gun_timer := $GunTimer
 onready var wait_timer := $WaitTimer
 onready var shoot_sfx := $ShootSFX
 onready var rotation_tween := $RotationTween
+onready var movement_tween := $MovementTween
+onready var minions_positions := $MinionsPositions
 #onready var shoot_positions_container := {
 #	1: $ShootPositions1,
 #	2: $ShootPositions2
@@ -46,7 +53,13 @@ onready var blackboard := {
 	"shoot_positions_container": {
 		1: $ShootPositions1,
 		2: $ShootPositions2
+	},
+	"current_shoot_stage": 1,
+	"defense_mode": {
+		"time": defense_time,
+		"time_over": false,
 	}
+	
 }
 
 #### Setters y Getters
@@ -69,11 +82,13 @@ func _ready() -> void:
 	can_shoot = true
 	self.allow_shoot = true
 	gun_timer.wait_time = shoot_rate
+	wait_timer.wait_time = blackboard.defense_mode.time
 	get_player()
-	current_shoot_positions_shooting = blackboard.shoot_positions_container[1]
+	current_shoot_positions_shooting = blackboard.shoot_positions_container[blackboard.current_shoot_stage]
 	if indestructible_bullets.size() > 0:
 		set_indestructible_bullet()
 
+# warning-ignore:unused_argument
 func _process(delta: float) -> void:
 	if is_aimer and not player == null and is_alive:
 		check_aim_to_player()
@@ -101,7 +116,8 @@ func set_indestructible_bullet() -> void:
 	for path in indestructible_bullets:
 		get_node(path).set_bullet_type(0)
 
-#func add_shoot_positions_to_container(key: int, shoot_positions: Node2D) -> void:
+func add_shoot_positions_to_container(key: int, shoot_positions: Node2D) -> void:
+	blackboard.shoot_positions_container[key] = shoot_positions
 #	shoot_positions_container[key] = shoot_positions
 
 func check_aim_to_player() -> void:
@@ -132,7 +148,27 @@ func look_at_center() -> void:
 	)
 	rotation_tween.start()
 
+func move_to_position(new_pos: Vector2) -> void:
+	movement_tween.interpolate_property(
+		self,
+		"global_position",
+		global_position,
+		new_pos,
+		2.0,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	movement_tween.start()
+
+# warning-ignore:unused_argument
+func spawn_minions(critic: bool, type: int) -> void:
+#	print("es critico: {c} y del tipo {t}".format({"c": critic, "t": type}))
+	pass
+
+
 func die() -> void:
+	$OffensiveBT.enable = false
+	$DefensiveBT.enable = false
 	is_aimer = false
 	can_shoot = false
 	gun_timer.stop()
@@ -144,7 +180,7 @@ func _on_GunTimer_timeout() -> void:
 	can_shoot = true
 
 func _on_WaitTimer_timeout() -> void:
-	set_offensive_mode()
+	blackboard.defense_mode.time_over = true
 
 #### Tareas
 func task_is_attacking(task) -> void:
@@ -166,6 +202,11 @@ func task_is_below_threshold(task) -> void:
 		task.failed()
 
 func task_next_shoot_stage(task) -> void:
+	blackboard.current_shoot_stage += 1
+	var shoot_stage:int = blackboard.current_shoot_stage
+	if shoot_stage > blackboard.shoot_positions_container.size():
+		shoot_stage = blackboard.shoot_positions_container.size()
+	current_shoot_positions_shooting = blackboard.shoot_positions_container[shoot_stage]
 	task.succeed()
 
 func task_next_move_stage(task) -> void:
@@ -178,10 +219,12 @@ func task_set_defense_mode(task) -> void:
 	print("CAMBIO A DEFENSA")
 	task.succeed()
 
-func set_offensive_mode() -> void:
-	$OffensiveBT.enable = false
-	$DefensiveBT.enable = true
+func task_set_offensive_mode(task) -> void:
+	$DefensiveBT.enable = false
+	$OffensiveBT.enable = true
+	blackboard.defense_mode.time_over = false
 	print("CAMBIO A ATAQUE")
+	task.succeed()
 
 func task_toggle_aim(task) -> void:
 	if task.get_param(0) == "true":
@@ -197,23 +240,57 @@ func task_disable_shooting(task) -> void:
 	gun_timer.stop()
 	task.succeed()
 
+func task_enable_shooting(task) -> void:
+	allow_shoot = true
+	can_shoot = true
+	task.succeed()
+
 func task_has_shield(task) -> void:
 	for child in get_children():
 		if child is EnemyShield:
-			print("shield")
 			task.succeed()
 			return
-	print("no shield")
 	task.failed()
 
 func task_set_shield(task) -> void:
-	print("set shield")
 	var size = task.get_param(0)
 	var new_shield := shield.instance()
 	new_shield.position = $ShieldPosition.position
 	new_shield.scale = Vector2(size, size)
 	new_shield.name = "EnemyShield"
 	add_child(new_shield)
+	task.succeed()
+
+func task_disabled_shield(task) -> void:
+	get_node_or_null("EnemyShield").queue_free()
+	task.succeed()
+
+func task_move_to_defense_position(task) -> void:
+	move_to_position(defense_position)
+	task.succeed()
+
+func task_is_critic(task) -> void:
+	if blackboard.tresholds[blackboard.current_treshold][0] in ["dead_life", "critic_life"]:
+		task.succeed()
+	else:
+		task.failed()
+
+func task_is_defense_time_over(task) -> void:
+	if blackboard.defense_mode.time_over:
+		task.succeed()
+	else:
+		task.failed()
+
+func task_spawn_minion(task) -> void:
+	var critic = task.get_param(0)
+	var type = task.get_param(1)
+	if critic:
+		print("normal")
+		spawn_minions(false, type)
+	else:
+		print("critico")
+		spawn_minions(true, type)
+	
 	task.succeed()
 
 
--- game/enemies/group4/BossG4.gd
+++ game/enemies/group4/BossG4.gd
@@ -1,1 +1,17 @@
 extends EnemyBossBT
+
+
+
+func spawn_minions(critic: bool, type: int) -> void:
+	if critic:
+		for pos in minions_positions.get_children():
+			var new_minion:EnemyBase = critic_minions[type].instance()
+			new_minion.global_position = pos.position
+			new_minion.set_can_shoot(true)
+			new_minion.set_allow_shoot(true)
+			new_minion.set_inside_play_screen(true)
+			get_parent().add_child(new_minion)
+	else:
+		for i in range(4):
+			var new_minion:EnemyBeamer = normal_minions[i].instance()
+			get_parent().add_child(new_minion)
