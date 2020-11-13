a34ee1058147cca226af485704b71ba0b66cc436
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-11-13 10:41:31 -0300

    fix puntero mouse presentacion

--- game/ui/menus/Presentation.gd
+++ game/ui/menus/Presentation.gd
@@ -2,9 +2,11 @@ extends Control
 
 export var next_scene := "res://game/ui/menus/Menu.tscn"
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 func change_scene() -> void:
 	get_tree().change_scene(next_scene)
-
 
 func _on_VideoPlayer_finished() -> void:
 	yield(get_tree().create_timer(2.0), "timeout")
